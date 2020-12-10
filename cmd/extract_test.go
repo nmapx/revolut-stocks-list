b9cd019ad563df59bc252ab7e406119f80ae35c8
nmapx <mt@ngen.pl>
2020-12-10 21:04:04 +0100

    Updated example screenshot, fixed tests

--- cmd/extract_test.go
+++ cmd/extract_test.go
@@ -31,29 +31,10 @@ func TestExtract(t *testing.T) {
 	whT := "QWERTYUIOPASDFGHJKLZXCVBNM"
 	whN := ""
 
-	expected = `SCCO,Southern Copper
-HEI,Heico
-SIRI,Sirius XM
-HES,Hess Corporation
-COLD,Americold Realty Trus
-NRG,NRG Energy
-HFC,HollyFrontier
-SGMO,Sangamo Therapeutic
-AZO,AutoZone
-HGV,Hilton Grand Vacation
-WEX,WEX
-NUE,Nucor
-SENS,Senseonics Holdings
-DAN,Dana Holding
-STLD,Steel Dynamics
-AMTD,TD Ameritrade
-DNKN,Dunkin' Brands Group
-EQNR,Equinor
-NWL,Newell Rubbermaid
-FHN,First Horizon National...Corporation
-URBN,Urban Outfitters
-PSTG,Pure Storage
-SBH,Sally Beauty
+	expected = `JMIA,Jumia
+SPG,Simon Property
+COTY,Coty
+CLNY,Colony Capital
 `
 	csv, _ = extract(in, out, langs, whT, whN)
 	if csv.String() != expected {
